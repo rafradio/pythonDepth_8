b472b70486c47194fa4ec7b475488eda0864ce41
Rafail Abdyushev <rafaelab@mail.ru>
2023-07-04 15:12:55 +0300

    main

---
+++ mainProg.py
@@ -0,0 +1,15 @@
+import sys
+from Serialisation import Serialisation as SRLZ
+from DumpToFiles import DumpToFiles as DMPF
+
+def main(args):
+    data = args[1] if len(args) > 1 else 'firstdir'
+    srlz = SRLZ(data)
+    srlz.walkDir(data)
+    dmpf = DMPF(srlz.dictFolders, data)
+    dmpf.dumpToJson()
+    # for key, value in srlz.dictFolders.items():
+    #     print(f'{key}: {value}')
+
+if __name__ == '__main__':
+    main(sys.argv)
